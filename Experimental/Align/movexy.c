dfceafab797700fea2a11b5c67eb1bfe31367b62
Philippe THIRION <philippe.thirion@laposte.net>
2016-11-01 22:08:02 +0100

    add movexy

---
+++ Experimental/Align/movexy.c
@@ -0,0 +1,124 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <dirent.h>
+#include "fitsio.h"
+
+long width=0;
+long height=0;
+
+unsigned short* a;
+unsigned short* c;
+
+int read_fits(char* path, unsigned short** image)
+{
+	fitsfile *fptr;    
+    int status = 0;
+    unsigned int bitpix, naxis;
+    long naxes[2];
+    long nelements;
+    int anynul;
+	if (!fits_open_file(&fptr, path, READONLY, &status))
+    {
+		if (!fits_get_img_param(fptr, 2, &bitpix, &naxis, naxes, &status) )
+        {
+			printf("bitpix=%d naxes=%d width=%ld height=%ld\n",bitpix,naxis,naxes[0],naxes[1]);
+			nelements = naxes[0]*naxes[1];
+			if (width==0 && height==0){
+				width = naxes[0];
+				height = naxes[1];
+			} else {
+				if (width != naxes[0] || height != naxes[1]){
+					printf("naxes error: %ldx%ld instead of %ldx%ld\n",naxes[0],naxes[1],width,height);
+					return -1;
+				}
+			}
+			
+			*image = malloc(sizeof(unsigned short)*nelements);
+			
+			if (bitpix == 16){
+				fits_read_img(fptr,TUSHORT,1,nelements,NULL,*image, &anynul, &status);
+				fits_close_file(fptr, &status);
+				if (status != 0) printf("status = %d\n",status);
+				return status;
+			} 
+			
+			if (bitpix == 8){
+				unsigned char* image8;
+				int i;
+				image8 = malloc(sizeof(unsigned char)*nelements);
+				fits_read_img(fptr,TBYTE,1,nelements,NULL,image8, &anynul, &status);
+				fits_close_file(fptr, &status);
+				if (status != 0) printf("status = %d\n",status);
+				// convert from 8 bit to 16 bit
+				for(i=0; i < nelements; i++)
+					(*image)[i]=((unsigned short)image8[i]);
+				return status;
+			} 
+			
+		  }
+     }
+     return -1;		
+}
+
+int write_fits(char* path,unsigned short* image)
+{
+	fitsfile *fptrout;
+	unsigned int naxis = 2;
+    long naxes[2];
+    int status = 0;
+    long nelements = width*height;
+    naxes[0] = width;
+	naxes[1] = height;
+	fits_create_file(&fptrout,path, &status);
+	fits_create_img(fptrout, SHORT_IMG, naxis, naxes, &status);
+	fits_write_img(fptrout, TUSHORT, 1, nelements, image, &status);
+	fits_close_file(fptrout, &status);
+}
+
+int main(int argc, char* argv[]) {
+
+
+	long nelements;
+	
+	int x,y;
+	int x0, y0;
+	
+	int xoffset, yoffset;
+	
+	
+	if (argc != 5){
+		printf("Usage: %s <in> <out> <x offset> <y offset>\n",argv[0]);
+		return -1;
+	}
+	
+	xoffset = atoi(argv[3]);
+	yoffset = atoi(argv[4]);
+	
+	printf("(%d,%d)\n",xoffset,yoffset);
+	
+	if (read_fits(argv[1],&a) == 0){
+		nelements = height * width;
+		printf("%ld\n", nelements);
+		c = malloc(sizeof(unsigned short)*nelements);
+		for(y=0;y< height;y++)
+			for(x=0;x< width; x++){
+				x0=x-xoffset;
+				y0=y-yoffset;
+				if ( ( x0 >= width ) || ( x0 < 0) ||  ( y0 >= height ) || (y0 < 0) ){
+						c[x+y*width] = 0;
+				} else {
+						c[x+y*width] = a[x0+y0*width];
+				}
+			}
+		   printf("%s\n",argv[2]);
+		   remove(argv[2]);
+		   if (write_fits(argv[2],c) == 0){
+				free(a); free(c);
+				return 0;
+		}
+	}
+	free(a); free(c);
+}
