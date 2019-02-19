4d22fd0734d218b94920df045afd5db926a9c712
Philippe THIRION <philippe.thirion@laposte.net>
2019-02-19 16:01:35 +0000

    add script to convert

---
+++ Cr2/cr2tofits.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import shutil
+import re
+
+
+def mkdir(d):
+	if not os.path.isdir(d):
+		os.mkdir(d)
+		
+def w(d):
+	return "cr2" + os.sep + d
+
+if not os.path.isdir("cr2"):
+	print "No cr2"
+	exit(0);
+
+def convert(directory):
+	if os.path.isdir(w(directory)):
+		os.mkdir(directory)
+		for root, dirs, files in os.walk(w(directory)): 
+			for name in files:
+				base,ext = os.path.splitext(name)
+				if (ext != ".pgm"):
+					source = root + os.sep + name
+					cmd = "dcraw -d -4 %s" % source
+					os.system(cmd)
+					source = root + os.sep + base + ".pgm"
+					target = directory + os.sep + base + ".fits"	
+					cmd = "./bin/pgm2fits %s %s" % (source,target)
+					os.system(cmd)
+					
+convert("light")
+convert("dark")
+convert("flat")
+convert("darkflat")
+						
+			
